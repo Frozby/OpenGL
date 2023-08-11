b205f1bcfb94172a843d7bca61e50462805bcd52
Frozby <branec298@gmail.com>
2023-08-11 08:31:10 +0200

    start of mesh, no idea where i left off

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,9 +1,6 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include "shader.h"
 #include "stb_image.h"
 #include "camera.h"
--- OpenGL/shader.h
+++ OpenGL/shader.h
@@ -9,6 +9,10 @@
 #include <sstream>
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 class Shader {
 public:
 	//the program ID
